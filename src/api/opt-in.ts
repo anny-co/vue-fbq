329e60ca6ad24df6c90c002e17975175ca61c4ee
Alexander Bartolomey <occloxium@gmail.com>
2021-07-17 18:38:09 +0200

    feat: actually do something on optIn
    * sends init event
    * sends track event
    
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- src/api/opt-in.ts
+++ src/api/opt-in.ts
@@ -1,5 +1,13 @@
 import disable from "./disable";
+import initialize from "./initialize";
+import track from "./track";
 
 export default (): void => {
   disable(false);
+
+  // spawn initial events:
+  // fbq('init', 'ID');
+  // fbq('track', 'PageView');
+  initialize();
+  track();
 };
