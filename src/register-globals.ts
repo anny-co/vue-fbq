6c2d178e5cf31d2597197056e8528b80c0f8ae76
Alexander Bartolomey <occloxium@gmail.com>
2021-07-17 17:45:57 +0200

    feat: add function for registering global objects on window context
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ src/register-globals.ts
@@ -0,0 +1,23 @@
+import { isBrowser } from "./utils";
+import { getOptions } from "./options";
+
+export default (): void => {
+  if (!isBrowser()) {
+    return;
+  }
+
+  const { globalObjectName, globalDataLayerName } = getOptions();
+
+  if (window[globalObjectName] == null) {
+    window[globalDataLayerName].queue = window[globalDataLayerName].queue || [];
+    window[globalDataLayerName].loaded = true;
+    window[globalDataLayerName].version = "2.0";
+    window[globalObjectName] = function (...args: unknown[]) {
+      window[globalDataLayerName].queue.push(args);
+    };
+  }
+
+  window[globalObjectName]("js", new Date());
+
+  return window[globalObjectName];
+};
