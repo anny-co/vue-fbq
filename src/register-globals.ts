cfa3ec4100519dc18e94be6a7674a1709deed7e3
Alexander Bartolomey <occloxium@gmail.com>
2021-07-17 21:01:08 +0200

    fix: register-globals mirrors the facebook code snippet
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- src/register-globals.ts
+++ src/register-globals.ts
@@ -1,28 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 import { isBrowser } from "./utils";
 import { getOptions } from "./options";
 
-type CallMethod = (...args: unknown[]) => void;
-
-export default (): CallMethod | void => {
+export default (): void => {
   if (!isBrowser()) {
     return;
   }
 
-  const { globalObjectName, globalDataLayerName } = getOptions();
+  const { globalObjectName } = getOptions();
 
-  if (window[globalObjectName] == null) {
-    window[globalDataLayerName] = window[globalDataLayerName] || [];
-    window[globalObjectName] = Object.assign(
-      function (...args: unknown[]) {
-        window[globalDataLayerName].push(...args);
-      },
-      {
-        loaded: true,
-        version: "2.0",
-      }
-    );
+  // if (window.fbq) return;
+  // const fbq = window.fbq = function () {
+  //   fbq.callMethod ? fbq.callMethod.apply(fbq, arguments) : fbq.queue.push(arguments);
+  // };
+  // if (!window._fbq) window._fbq = fbq;
+  // fbq.push = fbq;
+  // fbq.loaded = !0;
+  // fbq.version = "2.0";
+  // fbq.queue = [];
+
+  if (window[globalObjectName]) {
+    // already loaded
+    return;
   }
+
+  const fbq = (window[globalObjectName] = function (...args: unknown[]) {
+    (<any>fbq).callMethod
+      ? (<any>fbq).callMethod.apply(fbq, args)
+      : (<any>fbq).queue.push(...args);
+  });
+
+  if (!window["_" + globalObjectName]) {
+    window["_" + globalObjectName] = fbq;
+  }
+
+  window[globalObjectName] = Object.assign(fbq, {
+    loaded: true,
+    version: "2.0",
+    queue: [],
+    push: fbq,
+  });
 
   return window[globalObjectName];
 };
